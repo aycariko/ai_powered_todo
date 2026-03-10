e1ebf8534f59a3cde556ffbfd843251e3a1e190d
aycariko <121198157+aycariko@users.noreply.github.com>
2026-03-10 14:02:44 +0300

    Refactor todo.py for improved readability and structure

--- Routers/todo.py
+++ Routers/todo.py
@@ -17,38 +17,34 @@ from langchain_core.messages import HumanMessage, AIMessage
 import markdown
 from bs4 import BeautifulSoup
 
-#herbir url nin başında todo ekler
 router = APIRouter(
     prefix="/todo",
     tags=["Todo"],
 )
-#render etmesini sağlar
-templates = Jinja2Templates(directory="templates")
 #clienttan gelen veri doğru olmalı validation yapıyoruz.
+templates = Jinja2Templates(directory="templates")
+
 class ToDoRequest(BaseModel):
     title: str = Field(min_length=1)
     description: str = Field(min_length=1)
     priority: int = Field(ge=1, le=10)
     completed: bool
 
-def get_db(): #bütün end pointler bu fonksyiona depend edecek.
+def get_db(): 
     db = SessionLocal()
     try:
-        yield db # istek geldiğinde bir database oturumu açar. memory leak i önler.istek bitince kapatır.
-        # return gibidir, generator functiondır birden fazla değer döndürür iterate eder.
+        yield db
     finally:
         db.close()
 
 db_dependency = Annotated[Session, Depends(get_db)]
-#bizim  kullanıcılarımız tarafından geliyor mu dependecny olarak kullanmak gerek?
 user_dependency = Annotated[dict, Depends(get_current_user)]
 
-def redirect_to_login():#token süresi dolmuşsa veya giriş yapmamışsa.
+def redirect_to_login():
     redirect_response = RedirectResponse(url="/auth/login-page",status_code=status.HTTP_302_FOUND)
     redirect_response.delete_cookie("access_token")
     return redirect_response
-@router.get("/todo-page") # Çerezlerden (cookies/request) token'ı alıp kullanıcıyı doğrular. Veritabanından o kullanıcıya ait
-# Todo'ları çeker ve bunları HTML içine gömer.
+@router.get("/todo-page") 
 async def render_todo_page(request: Request, db: db_dependency):
     try:
         user = await get_current_user(request.cookies.get('access_token'))
@@ -68,7 +64,6 @@ async def render_add_todo_page(request: Request):
         return templates.TemplateResponse("add-todo.html", {"request": request, "user": user})
     except:
         return redirect_to_login()
-
 
 @router.get("/edit-todo-page/{todo_id}")
 async def render_edit_todo_page(request: Request, todo_id: int, db: db_dependency):
@@ -105,7 +100,7 @@ async def create_todo(user : user_dependency,db:db_dependency, todo_request: ToD
         description=todo_request.description,
         completed=todo_request.completed,
         priority=todo_request.priority,
-        owner_id= user.get('id') # sahip olanılan id ye göre create yapılabilecek.
+        owner_id= user.get('id') 
     )
     todo.description= create_todo_with_gemini(todo.description)
     db.add(todo)
@@ -142,8 +137,7 @@ def markdown_to_text(markdown_string):
     soup = BeautifulSoup(html, "html.parser")
     text = soup.get_text()
     return text
-
-
+    
 def create_todo_with_gemini(todo_string: str):
     load_dotenv()
     genai.configure(api_key=os.environ.get('GOOGLE_API_KEY'))
